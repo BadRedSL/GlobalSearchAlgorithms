db37c51e1bd7ffc3ca3535c98bb6debbd71f4c80
Alex_Ershov <AlexWE36@mail.ru>
2021-11-22 15:41:48 +0300

    inefficient parallel algorithm

--- AGS_cursovaya_v2/AGS_cursovaya_v2.cpp
+++ AGS_cursovaya_v2/AGS_cursovaya_v2.cpp
@@ -5,153 +5,14 @@
 #include <queue>
 #include <utility>
 #include <chrono>
+#include <thread>
 
 #include "Point.h"
 #include "Functions.h"
 #include "ForR.h"
 
-
-double minValueOld(const double a, const double b, const double r, const double accuracy,
-	size_t nMax, double (*fnc)(double))
-{
-	auto start = std::chrono::system_clock::now();
-
-	std::map<Point, double> set;
-	std::priority_queue<double, std::vector<double>, std::less<double>> rQueue;
-	std::priority_queue<double, std::vector<double>, std::less<double>> mQueue;
-
-	double min;
-	(fnc(a) < fnc(b)) ? min = fnc(a) : min = fnc(b);
-
-	Point rightPoint(b, fnc(b));
-	Point leftPoint(a, fnc(a));
-	Point newPoint(b, fnc(b));
-	set.emplace(leftPoint, 0);
-	auto curIter = set.emplace(rightPoint, 0);
-
-	double M = abs((rightPoint.y - leftPoint.y) / (rightPoint.x - leftPoint.x));
-	double prevM = M;
-
-	mQueue.push(M);
-
-	double m = (M > 0) ? r * M : 1;
-
-	double R = (m * (rightPoint.x - leftPoint.x)) + ((rightPoint.y - leftPoint.y) * (rightPoint.y - leftPoint.y) /
-		(m * (rightPoint.x - leftPoint.x))) - 2 * (rightPoint.y + leftPoint.y);
-
-	rQueue.push(R);
-
-	size_t count = 0;
-	
-	while ((count < nMax) && (rightPoint.x - leftPoint.x > accuracy))
-	{
-		double newX = ((rightPoint.x + leftPoint.x) / 2) - 
-			((rightPoint.y - leftPoint.y) / (2 * m));
-
-		double newY = fnc(newX);
-
-		newPoint.setPoint(newX, newY);
-
-		if (newY < min)
-		{
-			min = newY;
-		}
-
-		curIter = set.emplace(newPoint, 0);
-	
-		auto curTmp = curIter.first;
-		auto prevTmp = std::prev(curTmp);
-
-		if (mQueue.top() == abs((rightPoint.y - leftPoint.y) / (rightPoint.x - leftPoint.x)))
-		{
-			mQueue.pop();
-		}		
-
-		for (size_t i = 0; i < 2; ++i)
-		{
-			M = abs(((*curTmp).first.y - (*prevTmp).first.y) / 
-				((*curTmp).first.x - (*prevTmp).first.x));
-
-			mQueue.push(M);
-			++curTmp;
-			++prevTmp;
-		}
-
-		if (mQueue.top() > 0)
-		{
-			m = r * mQueue.top();
-		}
-		else
-		{
-			m = 1;
-		}
-
-		if (M == prevM)
-		{
-			curTmp = curIter.first;
-			prevTmp = std::prev(curTmp);
-
-			for (size_t i = 0; i < 2; ++i)
-			{
-				R = (m * ((*curTmp).first.x - (*prevTmp).first.x)) +
-					((((*curTmp).first.y - (*prevTmp).first.y) *
-						((*curTmp).first.y - (*prevTmp).first.y)) /
-						(m * (*curTmp).first.x - (*prevTmp).first.x)) -
-					(2 * (*curTmp).first.y + (*prevTmp).first.y);
-				++curTmp;
-				++prevTmp;
-				rQueue.push(R);
-
-				if (rQueue.top() == R)
-				{
-					rightPoint = (*curTmp).first;
-					leftPoint = (*prevTmp).first;
-				}
-
-			}
-		}
-		else
-		{
-			while (!rQueue.empty())
-			{
-				rQueue.pop();
-			}
-
-			for (curTmp = (++set.begin()), prevTmp = set.begin(); curTmp != set.end(); ++curTmp, ++prevTmp)
-			{
-				R = (m * ((*curTmp).first.x - (*prevTmp).first.x)) +
-					((((*curTmp).first.y - (*prevTmp).first.y) *
-						((*curTmp).first.y - (*prevTmp).first.y)) /
-						(m * (*curTmp).first.x - (*prevTmp).first.x)) -
-					(2 * (*curTmp).first.y + (*prevTmp).first.y);
-
-				rQueue.push(R);
-
-				if (rQueue.top() == R)
-				{
-					rightPoint = (*curTmp).first;
-					leftPoint = (*prevTmp).first;
-				}
-
-			}
-
-		}
-
-		prevM = M;
-		std::cout << count << std::endl;
-		++count;
-	}
-
-	auto end = std::chrono::system_clock::now();
-	std::chrono::duration<double> sec = end - start;
-	std::cout << "Время работы алгоритма: " << sec.count() << " сек " << std::endl;
-
-	return min;
-}
-
-
 double minValue(const double a, const double b, const double r, const double accuracy,
-	size_t nMax, double (*fnc)(double))
+	size_t nMax, double (*fnc)(double), double &result)
 {
 	auto start = std::chrono::system_clock::now();
 
@@ -286,8 +147,10 @@ double minValue(const double a, const double b, const double r, const double acc
 
 	auto end = std::chrono::system_clock::now();
 	std::chrono::duration<double> sec = end - start;
-	std::cout << "Время работы алгоритма: " << sec.count() << " сек " << std::endl;
 
+	//std::cout << "Время работы алгоритма: " << sec.count() << " сек " << std::endl;
+
+	result = minX;
 	return minX;
 }
 
@@ -295,21 +158,52 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 
-	//std::cout << minValue(2.7, 7.5, 4.29, 0.001, 1000, function1); 
-	//std::cout << minValue(0, 10, 67, 0.001, 1000, function2); 
-	//std::cout << minValue(0, 1.2, 36, 0.001, 1000, function3); 
-	//std::cout << minValue(-10, 10, 2.5, 0.001, 1000, function4); 
-	//std::cout << minValue(2.7, 7.5, 6, 0.001, 1000, function5); 
-	//std::cout << minValue(0, 4, 6.5, 0.001, 1000, function7);
-	//std::cout << minValue(-5, 5, 6.5, 0.001, 1000, function8);
-	//std::cout << minValue(0, 6.5, 4, 0.00001, 1000, function9);
-	//std::cout << minValue(-3, 3, 85, 0.001, 1000, function10);
+	size_t numThread = 12;
+	double leftBorder = 2.7;
+	double rightBorder = 7.5;
+	double r = 4.29;
+	double accuracy = 0.0001;
+	size_t maxIteration = 10000;
+	double (*fnc)(double) = function1;
+	
+	double border = leftBorder;
+	double step = (rightBorder - leftBorder) / numThread;
+	std::vector<std::thread> vThread;
+	std::vector<double> vResult(numThread, 0);
+	double minX = INT_MAX;
+	double minY = INT_MAX;
 
-	//std::cout << minValue(0, 6.5, 4, 0.00000000001, 12000, function9);//329 sec debug-mode
-	//std::cout << minValueOld(0, 6.5, 4, 0.00000000001, 12000, function9);//408 sec debug-mode
+	auto start = std::chrono::system_clock::now();
 
-	std::cout << minValue(0, 6.5, 4, 0.00000000001, 12000, function9);//9 sec release-mode
-	//std::cout << minValueOld(0, 6.5, 4, 0.00000000001, 12000, function9);//10,5 sec release-mode
+	for (size_t i = 0; i < numThread; ++i)
+	{
+		vThread.push_back(std::thread(minValue, border, border + step, r, accuracy, maxIteration, fnc, std::ref(vResult[i])));
+		border += step;
+
+	}
+
+	for (size_t i = 0; i < vThread.size(); ++i)
+	{
+		vThread[i].join();
+	}
+
+	std::cout << "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++" << std::endl;
+
+	for (const auto &i: vResult)
+	{
+		std::cout << "Полученная точка: " << i << std::endl;
+		if (fnc(i) < minY)
+		{
+			minY = fnc(i);
+			minX = i;
+		}
+	}
+
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<double> sec = end - start;
+	std::cout << "Время работы параллельного алгоритма: " << sec.count() << " сек " << std::endl;
+
+	std::cout <<"Х при котором достигается минимум: " << minX << std::endl;
 
 	return 0;
 }
