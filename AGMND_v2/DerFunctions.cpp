f57e89b8d56faa17c37365b37a3a70de50800ddf
Alex Ershov <AlexWE36@mail.ru>
2023-04-19 18:21:36 +0300

    first realisation of parallel version algorithm

--- AGMND_v2/DerFunctions.cpp
+++ AGMND_v2/DerFunctions.cpp
@@ -5,10 +5,10 @@ double derFunction1(const double x)
 {
 	double y = std::cos(x) + (10/3) * std::cos((10/3)*x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -22,10 +22,10 @@ double derFunction2(const double x)
 		y += (k * std::cos((k + 1) * x + k)*(k+1));
 	}
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -34,10 +34,10 @@ double derFunction3(const double x)
 {
 	double y = 18*(3*x-1.4)*std::cos(18*x)+3*std::sin(18*x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -46,10 +46,10 @@ double derFunction4(const double x)
 {
 	double y = 2*x*(x+std::sin(x))*std::exp(-(x*x))-(std::cos(x)+1)*std::exp(-(x*x));
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -58,10 +58,10 @@ double derFunction5(const double x)
 {
 	double y = -21 / 25 + 1 / x + 10 * std::cos((10 * x) / 3) / 3 + std::cos(x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -70,10 +70,10 @@ double derFunction7(const double x)
 {
 	double y = exp(-x) * sin((2 * M_PI) * x) - 2 * M_PI * cos((2 * M_PI) * x) * exp(-x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -82,22 +82,22 @@ double derFunction8(const double x)
 {
 	double y = ((-5 + 2 * x) / (x * x + 1) - (2 * x * (x * x - 5 * x + 6))) / ((x * x + 1) * (x * x + 1));
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
 
 double derFunction9(const double x)
 {
-	double y = x + 3 * std::cos(3 * x);
+	double y = 3 * std::cos(3 * x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -106,10 +106,10 @@ double derFunction10(const double x)
 {
 	double y = -6 + 2 * x + 2 * (x - 3) + x * std::exp((x * x) / 2);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -118,10 +118,10 @@ double derFunction11(const double x)
 {
 	double y = 2*x;
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
--- AGMND_v2/Functions.cpp
+++ AGMND_v2/Functions.cpp
@@ -5,10 +5,10 @@ double function1(const double x)
 {
 	double y = std::sin(x) + std::sin(10 * x / 3);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -22,10 +22,10 @@ double function2(const double x)
 		y += (k * std::sin((k + 1) * x + k));
 	}
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return (-y);
 }
@@ -34,10 +34,10 @@ double function3(const double x)
 {
 	double y = (3 * x - 1.4) * std::sin(18 * x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -46,10 +46,10 @@ double function4(const double x)
 {
 	double y = (-1) * (x + std::sin(x)) * std::exp(-(x * x));
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -58,10 +58,10 @@ double function5(const double x)
 {
 	double y = std::sin(x) + sin(10 * x / 3) + log(x) - 0.84 * x + 3;
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -70,10 +70,10 @@ double function7(const double x)
 {
 	double y = -std::sin(2 * M_PI * x) * std::exp(-x);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -82,10 +82,10 @@ double function8(const double x)
 {
 	double y = (x * x - 5 * x + 6) / (x * x + 1);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -94,10 +94,10 @@ double function9(const double x)
 {
 	double y = -x + std::sin(3 * x) - 1;
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -106,10 +106,10 @@ double function10(const double x)
 {
 	double y = 2 * (x - 3) * (x - 3) + std::exp((x * x) / 2);
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
@@ -118,10 +118,10 @@ double function11(const double x)
 {
 	double y = x*x;
 
-	//for (size_t i = 0; i < 100000; ++i)
-	//{
-	//	auto tmp = std::chrono::system_clock::now();
-	//}
+	for (size_t i = 0; i < 100000; ++i)
+	{
+		auto tmp = std::chrono::system_clock::now();
+	}
 
 	return y;
 }
